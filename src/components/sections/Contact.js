3a0eaa9d481984dc3281bb8c8dbf9fab10c92c9e
Luis <luishms10@gmail.com>
2022-08-28 11:34:32 -0500

    Commit: agregamos Re Capcha a formulario de contacto - 28/08/2022

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -19,6 +19,9 @@ import emailjs from '@emailjs/browser';
 import { PUBLIC_KEY, SERVICE_ID, TEMPLATE_ID } from '../../utils/constants';
 import Swal from 'sweetalert2'
 
+//re-capcha
+import ReCAPTCHA from "react-google-recaptcha";
+
 
 
 const propTypes = {
@@ -101,105 +104,111 @@ const Contact = ({
                 height={50} />
             </a>
             <a href="https://github.com/luhemusa" target="_blank" style={{ cursor: "pointer" }}>
-            <Image
-              src={require('./../../assets/images/gmail.png')}
-              alt="Features split 01"
-              width={50}
-              height={50}
-            />
-          </a>
-          <a href="https://github.com/luhemusa" target="_blank" style={{ cursor: "pointer" }}>
-            <Image
-              src={require('./../../assets/images/github.png')}
-              alt="Features split 01"
-              width={50}
-              height={50} />
-          </a>
+              <Image
+                src={require('./../../assets/images/gmail.png')}
+                alt="Features split 01"
+                width={50}
+                height={50}
+              />
+            </a>
+            <a href="https://github.com/luhemusa" target="_blank" style={{ cursor: "pointer" }}>
+              <Image
+                src={require('./../../assets/images/github.png')}
+                alt="Features split 01"
+                width={50}
+                height={50} />
+            </a>
+
+          </div>
+
+          <br />
+
+          <div className="reveal-from-bottom" data-reveal-delay="400">
+            <Formik
+              initialValues={{
+                name: '',
+                email: '',
+                text: '',
+                recapcha: false
+              }}
+
+              validationSchema={Yup.object().shape({
+
+                name: Yup.string().required('Requerido'),
+                email: Yup.string().email().required('Requerido'),
+                text: Yup.string().required('Requerido'),
+                recapcha: Yup.boolean().required('Requerido')
+
+              })}
+
+              onSubmit={(fields, { resetForm }) => {
+
+                emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+                  .then((result) => {
+                    Swal.fire(
+                      result.text,
+                      'Mensaje Enviado!',
+                      'success'
+                    )
+                    resetForm();
+                    console.log(result.text);
+                  }, (error) => {
+                    console.log(error.text);
+                  });
+
+                console.log(fields)
+              }} >
+
+              {({ values, errors, status, touched, setFieldValue, onSubmit }) => (
+
+                <div className="card-body">
+                  <Form className="form-main" ref={form}>
+
+                    <div className="form-contact">
+                      <FormGroup>
+                        <Label for="exampleEmail">
+                          Nombre
+                        </Label>
+                        <Field className='form-control' name="name" placeholder="Ingrese Nombre completo" />
+                        <p style={{ color: "red" }}><ErrorMessage name="name" /></p>
+                      </FormGroup>
+                      <FormGroup>
+                        <Label for="exampleEmail">
+                          Correo electronico
+                        </Label>
+                        <Field className='form-control' name="email" placeholder="Ingrese Email" />
+                        <p style={{ color: "red" }}><ErrorMessage name="email" /></p>
+                      </FormGroup>
+                      <FormGroup>
+                        <Label for="exampleEmail">
+                          Texto
+                        </Label>
+                        <Field className='form-control' name="text" placeholder="Ingrese Texto" as="textarea" style={{ height: "150px" }} />
+                        <p style={{ color: "red" }}><ErrorMessage name="text" /></p>
+                      </FormGroup>
+                    </div>
+                    <ReCAPTCHA
+                      sitekey=" 6Lc787MhAAAAAKb4yIisSe6CazXJRW8ZfX7EFbj_"
+                      onChange={() => setFieldValue('recapcha', true)}
+                    />
+                     <br />
+                    <div>
+                      <Button varian="contained" type="submit" className="btn btn-primary me-1">Enviar</Button>
+                      <Button varian="contained" type="reset" className="btn btn-secondary">Reset</Button>
+                    </div>
+
+                  </Form>
+                </div>
+              )}
+
+            </Formik>
+          </div>
+
+          <br /><br />
+
 
         </div>
-
-        <br />
-
-        <div className="reveal-from-bottom" data-reveal-delay="400">
-          <Formik
-            initialValues={{
-              name: '',
-              email: '',
-              text: '',
-            }}
-
-            validationSchema={Yup.object().shape({
-
-              name: Yup.string().required('Requerido'),
-              email: Yup.string().email().required('Requerido'),
-              text: Yup.string().required('Requerido')
-
-            })}
-
-            onSubmit={(fields, { resetForm }) => {
-
-              emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
-              .then((result) => {
-                Swal.fire(
-                  result.text,
-                  'Mensaje Enviado!',
-                  'success'
-                )
-                 resetForm();
-                  console.log(result.text);
-              }, (error) => {
-                  console.log(error.text);
-              });
-
-              console.log(fields)
-            }} >
-
-            {({ values, errors, status, touched, setFieldValue, onSubmit }) => (
-
-              <div className="card-body">
-                <Form className="form-main" ref={form}>
-
-                  <div className="form-contact">
-                    <FormGroup>
-                      <Label for="exampleEmail">
-                        Nombre
-                      </Label>
-                      <Field className='form-control' name="name" placeholder="Ingrese Nombre completo" />
-                      <p style={{ color: "red" }}><ErrorMessage name="name" /></p>
-                    </FormGroup>
-                    <FormGroup>
-                      <Label for="exampleEmail">
-                        Correo electronico
-                      </Label>
-                      <Field className='form-control' name="email" placeholder="Ingrese Email" />
-                      <p style={{ color: "red" }}><ErrorMessage name="email" /></p>
-                    </FormGroup>
-                    <FormGroup>
-                      <Label for="exampleEmail">
-                        Texto
-                      </Label>
-                      <Field className='form-control' name="text" placeholder="Ingrese Texto" as="textarea" style={{ height: "150px" }}/>
-                      <p style={{ color: "red" }}><ErrorMessage name="text" /></p>
-                    </FormGroup>
-                  </div>
-                  <br />
-                  <div>
-                    <Button varian="contained" type="submit" className="btn btn-primary me-1">Enviar</Button>
-                    <Button varian="contained" type="reset" className="btn btn-secondary">Reset</Button>
-                  </div>
-
-                </Form>
-              </div>
-            )}
-
-          </Formik>
-        </div>
-
-        <br /><br />
-
-
       </div>
-    </div>
     </section >
   );
 }
